e85d8690d106d9389900fd95850d8ced0082268f
ZCplayground <250151700@qq.com>
2017-06-11 17:12:42 +0800

    C++ version and GUI version

--- 四则运算/四则运算/Resource.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include<malloc.h>
-#define MAXLINE 30 //资源文件内的最大行数
-#define MAXLEN 200 //每行的最大字符数
-
-//存储资源文件内容的指针数组
-char *Resource[MAXLINE] = { (char *)malloc(MAXLEN * sizeof(char)) };
